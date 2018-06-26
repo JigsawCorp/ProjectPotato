7abcebba515ed03b8b9c896243a50bbe846c00b9
root <root@LAPTOP-NV0TIEGH.localdomain>
2018-06-26 18:14:14 -0400

    Added more constructors for Exercise

--- Jigsaw/app/src/main/java/com/jigsawcorp/android/jigsaw/Model/PerformedExercise.java
+++ Jigsaw/app/src/main/java/com/jigsawcorp/android/jigsaw/Model/PerformedExercise.java
@@ -1,5 +1,6 @@
 package com.jigsawcorp.android.jigsaw.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,6 +13,29 @@ public class PerformedExercise {
     public PerformedExercise(Exercise exercise, Date date) {
         mExercise = exercise;
         mDate = date;
+        mNotes = "";
+        mSets = new ArrayList<>();
+    }
+
+    public PerformedExercise(Exercise exercise, Date date, String notes) {
+        mExercise = exercise;
+        mDate = date;
+        mNotes = notes;
+        mSets = new ArrayList<>();
+    }
+
+    public PerformedExercise(Exercise exercise, Date date, List<Set> sets) {
+        mExercise = exercise;
+        mDate = date;
+        mNotes = "";
+        mSets = sets;
+    }
+
+    public PerformedExercise(Exercise exercise, Date date, List<Set> sets, String notes) {
+        mExercise = exercise;
+        mDate = date;
+        mNotes = notes;
+        mSets = sets;
     }
 
     public Exercise getExercise() {
