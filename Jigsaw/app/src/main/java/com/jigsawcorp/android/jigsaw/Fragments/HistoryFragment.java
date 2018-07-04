de0c60a27247c45cc9b9215ce173f2936ff961fb
Jigsaw <philbond1997@hotmail.com>
2018-07-04 14:55:16 -0400

    Added TabLayout to the history fragment. The layout has a function ViewPager to slide between fragments. It currently has two empty tabs

--- Jigsaw/app/src/main/java/com/jigsawcorp/android/jigsaw/Fragments/HistoryFragment.java
+++ Jigsaw/app/src/main/java/com/jigsawcorp/android/jigsaw/Fragments/HistoryFragment.java
@@ -1,14 +1,24 @@
 package com.jigsawcorp.android.jigsaw.Fragments;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jigsawcorp.android.jigsaw.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoryFragment extends Fragment {
+    private ViewPager mViewPager;
     @Override
     public void onCreate(Bundle savecInstanceState) {
         super.onCreate(savecInstanceState);
@@ -16,7 +26,21 @@ public class HistoryFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle onSavedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_history, container, false);
+
+        // Setup view Pager
+        mViewPager = (ViewPager) v.findViewById(R.id.fragment_history_view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new HistoryCalendarTab(), "Calendar");
+        adapter.addFragment(new HistoryRecentTab(), "Recent");
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = v.findViewById(R.id.fragment_history_tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.setupWithViewPager(mViewPager);
+
+
         return v;
     }
 
@@ -24,5 +48,35 @@ public class HistoryFragment extends Fragment {
     public void onResume() {
         super.onResume();
         getActivity().setTitle("History");
+    }
+
+    // Adapter for the viewpager using FragmentPagerAdapter
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
